084e11e9bc1dbd744a13ff3e4bbb6052b1c45ff8
Gudni Geir Agnarsson <gudnieir@gmail.com>
2024-03-17 14:22:32 +0000

    breyting hotfix

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-apfrom flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Halló heimur!"
+    return "Halló frá hotfix"
 
 if __name__ == "__main__":
     app.run(debug=True)
